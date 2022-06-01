6dcef097822896c3ff7bac539316a31ee8fae3e0
Aaron Hazzard <82277875+TechBliz@users.noreply.github.com>
2022-06-01 11:24:39 -0400

    Added home Slide Show

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+function scrollright(){
+    console.log("clicked");
+var left = document.querySelector("#image-wrapper");
+left.scrollBy(800, 0);
+
+}
+
+function scrollleft(){
+var right = document.querySelector("#image-wrapper");
+right.scrollBy(-800, 0);
+
+}
