930a9c62b8e780405bb30b95a67d18d47e855ded
Aaron Hazzard <82277875+TechBliz@users.noreply.github.com>
2022-06-01 20:07:00 -0400

    Updated laptop view

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,22 @@
 function scrollright(){
-    console.log("clicked");
-var left = document.querySelector("#image-wrapper");
-left.scrollBy(800, 0);
+    if ($(window).width() >= 1024) {
+        console.log("correct");
+        var right = document.querySelector("#image-wrapper");
+        right.scrollBy(950, 0);
+        // write you css here for screen size is more then 768
+       }else if ($(window).width() > 768{
+        console.log("wrong");
+        var right = document.querySelector("#image-wrapper");
+        right.scrollBy(800, 0);
+       
+       // write you css here for screen size is less then 768
+       }
+   
 
 }
 
 function scrollleft(){
 var right = document.querySelector("#image-wrapper");
-right.scrollBy(-800, 0);
+right.scrollBy(-1125, 0);
 
 }
