09b9fe9e6b77f126c99de5119f2b401a74d8abdf
Aaron Hazzard <aaronsploit@gmail.com>
2022-07-05 23:37:38 -0800

    about us viewports

--- startover/javascript/functionality.js
+++ startover/javascript/functionality.js
@@ -223,3 +223,10 @@ function CalendarControl() {
   }
   
   const calendarControl = new CalendarControl();
+
+
+  const parallax = document.getElementById("first");
+window.addEventListener("scroll", function() {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.8 + "px";
+})
