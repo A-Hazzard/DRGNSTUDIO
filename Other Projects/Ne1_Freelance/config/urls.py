63f5d3ef08872dcfbd3a8e38d5b12199a0ad3ae9
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-01-06 20:32:56 -0400

    Revert "Fixed SQL and changed sequence for patient num"
    This reverts commit 31518c2c382f7012c6828e4e44d226676ac1983e.

--- Other Projects/Ne1_Freelance/config/urls.py
+++ Other Projects/Ne1_Freelance/config/urls.py
@@ -1,4 +1,4 @@
-"""Ne1_Freelance URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.views.generic import TemplateView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('display/', include('website.urls'))
 ]
---
+++ Other Projects/Ne1_Freelance/website/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def displayHello(request):
+    return render(request, 'hello.html', {'name' : 'Aaron'})
